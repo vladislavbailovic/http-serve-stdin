fe22fa97030c442494f9e77b666fd74ac42b7ad8
vladislav <ve@incsub.com>
2021-08-22 05:50:19 +0200

    Set default response headers

--- main.go
+++ main.go
@@ -19,11 +19,26 @@ func getStdin(std io.Reader) string {
 
 func getStdinHandler(std io.Reader) func(http.ResponseWriter, *http.Request) {
 	return func(resp http.ResponseWriter, req *http.Request) {
+		for name, value := range getDefaultHeaders() {
+			resp.Header().Set(name, value)
+		}
+		resp.WriteHeader(http.StatusOK)
+
 		fmt.Fprintf(resp, getStdin(std))
 	}
 }
 
+func getDefaultHeaders() map[string]string {
+	return map[string]string{
+		"content-type": "text/plain; charset=utf-8",
+	}
+}
+
+func serveStdin(port int, std io.Reader) {
+	http.HandleFunc("/", getStdinHandler(std))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
 func main() {
-	http.HandleFunc("/", getStdinHandler(os.Stdin))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	serveStdin(8080, os.Stdin)
 }
--- main_test.go
+++ main_test.go
@@ -29,8 +29,36 @@ func TestGetStdinReturnsEmptyStringIfNeeded(t *testing.T) {
 	}
 }
 
+func TestGetDefaultHeaders(t *testing.T) {
+	headers := getDefaultHeaders()
+
+	if headers["content-type"] != "text/plain; charset=utf-8" {
+		t.Log(headers)
+		t.Fatalf("expected text as default content type, got %s", headers["content-type"])
+	}
+}
+
 func TestStdinHandlerResponse(t *testing.T) {
 	expected := "Hello there"
+	expectedReader := strings.NewReader(expected)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(getStdinHandler(expectedReader))
+	handler.ServeHTTP(recorder, req)
+
+	if actual := recorder.Body.String(); actual != expected {
+		t.Log(recorder)
+		t.Fatalf("expected server to respond with {%s}, got {%s} instead", expected, actual)
+	}
+}
+
+func TestStdinIsServedWithDefaultHeaders(t *testing.T) {
+	expected := ""
 	expectedReader := strings.NewReader(expected)
 
 	req, err := http.NewRequest("GET", "/", nil)
@@ -47,8 +75,8 @@ func TestStdinHandlerResponse(t *testing.T) {
 		t.Fatalf("expected success requesting stdin, got %d instead", recorder.Code)
 	}
 
-	if actual := recorder.Body.String(); actual != expected {
-		t.Log(recorder)
-		t.Fatalf("expected server to respond with {%s}, got {%s} instead", expected, actual)
+	if contentType := recorder.Header().Get("Content-Type"); contentType != getDefaultHeaders()["content-type"] {
+		t.Log(recorder.Header())
+		t.Fatalf("expected {%s} content type, got {%s} instead", getDefaultHeaders()["content-type"], contentType)
 	}
 }
